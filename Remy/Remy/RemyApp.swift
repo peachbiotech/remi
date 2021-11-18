c9463d4127d3c8eedd89ab95c344b95d8fbed125
Jia Chun Xie <jasonx@le-big-mac.wireless-1x.unc.edu>
2021-11-18 14:08:14 -0500

    init commit

---
+++ Remy/Remy/RemyApp.swift
@@ -0,0 +1,20 @@
+//
+//  RemyApp.swift
+//  Remy
+//
+//  Created by Jia Chun Xie on 11/18/21.
+//
+
+import SwiftUI
+
+@main
+struct RemyApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
